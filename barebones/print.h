0983e08a1dc1f555d7cb77f7de5c0eb7bccd51c4
Joel Williamson <joel.s.williamson@gmail.com>
2013-10-23 15:11:06 -0400

    Fixed printing to handle sectors properly

--- barebones/print.h
+++ barebones/print.h
@@ -1,10 +1,10 @@
 # print takes the address of the c-string to print
-# in %es:(%bx)
+# in (%bx)
 print:
 	push %ax
 	push %bx
 print_loop:
-	movb %es:(%bx), %al	# Get the next byte
+	movb (%bx), %al	# Get the next byte
 	cmpb $0x00,%al
 	je end_print
 	movb $0x0e,%ah
@@ -14,6 +14,27 @@ print_loop:
 	jmp print_loop
 
 end_print:
+	pop %bx
+	pop %ax
+	ret
+
+
+# print_es takes the address of the c-string to print
+# in %es:(%bx)
+print_es:
+	push %ax
+	push %bx
+print_loop_es:
+	movb %es:(%bx), %al	# Get the next byte
+	cmpb $0x00,%al
+	je end_print_es
+	movb $0x0e,%ah
+	int $0x10		# int 10,e - teletype output
+				#	%al - character to print
+	inc %bx
+	jmp print_loop_es
+
+end_print_es:
 	pop %bx
 	pop %ax
 	ret
